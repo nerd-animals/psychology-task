20e16ba9b3956f45fd69e720ca1d10a40f2da33a
SanGyuk-Raccoon <dmstjkim80@gmail.com>
2024-10-20 14:42:26 +0900

    fix: always trial session is existed

--- src/step/explain.tsx
+++ src/step/explain.tsx
@@ -12,11 +12,7 @@ export default function explain({
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
       <div>과제 설명</div>
-      {appSetting.trialSession.taskList.length ? (
-        <Button label="연습 문제" onClick={() => setAppStep('trial')} />
-      ) : (
-        <Button label="과제 시작" onClick={() => setAppStep('task')} />
-      )}
+      <Button label="과제 시작" onClick={() => setAppStep('task')} />
     </div>
   );
 }

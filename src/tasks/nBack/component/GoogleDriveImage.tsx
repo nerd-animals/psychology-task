68b1ed12ae3375cb298ce9b242d5f7aa1134a510
SanGyuk-Raccoon <dmstjkim80@gmail.com>
2024-11-28 11:03:59 +0900

    feat: default google drive images

--- src/tasks/nBack/component/GoogleDriveImage.tsx
+++ src/tasks/nBack/component/GoogleDriveImage.tsx
@@ -1,7 +1,9 @@
 export default function GoogleDriveImage(id: string, alt: string) {
   return (
     <img
-      src={`https://drive.google.com/thumbnail?id=${id}&sz=w1000`}
+      src={`https://drive.google.com/thumbnail?id=${id}&sz=w1280`}
+      width="1280"
+      height="720"
       alt={alt}
     />
   );
